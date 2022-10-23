ff68ce3e75477369bea46b9f24d49cb35a11e2ea
miekru <sajna.michal@gmail.com>
2022-10-23 09:00:20 +0200

    Add products render

--- src/js/script.js
+++ src/js/script.js
@@ -53,10 +53,32 @@
   };
 
   class Product{
-    constructor(){
+    constructor(id, data){
       const thisProduct = this;
 
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
       console.log('new Product:', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+
+      // generate HTML based on template
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      // create element using utils.createElementFromHTML
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      // find menu container
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      // add element to menu
+      menuContainer.appendChild(thisProduct.element);
+
     }
   }
 
